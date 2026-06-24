b4e34bf124c858f39a1abd0286e9fb25ae3010b8
Gabin Dubois <gabin.dubois@spikeelabs.fr>
2026-06-24 10:42:51 +0200

    add log to my_shutdown script

--- my_shutdown.sh
+++ my_shutdown.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-echo "Arrêt des VMs..."
+LOG_FILE="/var/log/ups.log"
+
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $*" >> "$LOG_FILE"
+}
+
+log "Arrêt des VMs..."
 
 virsh list --all --name | while read -r vm
 do
     [ -z "$vm" ] && continue
 
-    echo ""
+    log ""
     # Vérifie que la VM est démarrée
     if virsh domstate "$vm" | grep -q running
     then
-        echo "Arrêt de $vm"
+        log "Arrêt de $vm"
         virsh shutdown "$vm"
 
         while virsh domstate "$vm" | grep -q running
         do
-            echo "$vm en train de shutdown..."
+            log "$vm en train de shutdown..."
             sleep 2
         done
 
-        echo "$vm arrêtée"
+        log "$vm arrêtée"
     fi
 done
 
-echo ""
-echo "Arrêt du serveur dans 5s:"
-
-for i in $(seq 5 -1 0)
-do
-    echo "$i..."
-    sleep 1
-done
-
-echo "Arrêt du serveur"
+log ""
+log "Arrêt immédiat du serveur"
 shutdown -h now
